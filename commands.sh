d63eaa07a2327a0da09e6ceeff408df51425bd59
Sandeepcs55 <gangadharsandy@551>
2026-07-19 23:19:31 +0530

    git commands

---
+++ commands.sh
@@ -0,0 +1,8 @@
+# first clone a repo from github to git using git clone ..url
+# change the directory to the cloned rpo directory using cd
+# create a file using vi
+# add or edit the file
+# once edites/changes are made add the file using git add .
+# once added see the diff using git diff
+# commit the added file using git commit
+# push the file from local to remote using git push origin main
